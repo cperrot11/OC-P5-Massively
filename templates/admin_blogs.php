e644d8d611593368a4963091006f7eb404c1dfbf
PERROTIN Christophe <christophe@perrotin.eu>
2018-11-04 17:18:45 +0100

    admin_article Sort v2

--- templates/admin_blogs.php
+++ templates/admin_blogs.php
@@ -44,25 +44,23 @@ $this->title = "Accueil";
                     <th scope="col">Date</th>
                 </tr>
                 </thead>
+                <tbody>
                 <?php
                     foreach ($articles as $article)
                     {
                         ?>
-                            <tbody>
                             <tr class="table-light">
-                                <th scope="row"><?= htmlspecialchars($article->getId());?></th>
+                                <td scope="row"><?= htmlspecialchars($article->getId());?></td>
                                 <td><?= htmlspecialchars($article->getTitle());?></td>
                                 <td><?= htmlspecialchars($article->getAuthor());?></td>
                                 <td><?= htmlspecialchars($article->getDateAdded());?></td>
                                 <td><a href="../public/index.php?route=updateArticle&idArt=<?= htmlspecialchars($article->getId());?>">Modifier</a></td>
-
                                 <td><a href="../public/index.php?route=deleteArticle&idArt=<?= htmlspecialchars($article->getId());?>">Supprimer</a> </td>
                             </tr>
-                            </tbody>
-
                         <?php
                     }
                     ?>
+                </tbody>
             </table>
 
         </div>
@@ -70,32 +68,38 @@ $this->title = "Accueil";
     <script src="../public/js/jquery.js"></script>
     <script>
         $(document).ready(function() {
-            $('th').click(function(){
-
-                var table = $(this).parents('table').eq(0)
-                var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
-                this.asc = !this.asc
-                if (!this.asc){
-                    rows = rows.reverse();
-                    $(this).removeClass('asc');
-                    $(this).addClass('desc selected')
-                }
-                else
-                {
-                    $(this).removeClass('desc');
-                    $(this).addClass('asc selected');
-                }
-                $(this).siblings().removeClass('asc selected');
-                $(this).siblings().removeClass('desc selected');
-                for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-            })
-            function comparer(index) {
-                return function(a, b) {
-                    var valA = getCellValue(a, index), valB = getCellValue(b, index)
-                    return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
-                }
-            }
-            function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+            $('th').each(function(col) {
+                $(this).hover(
+                    function() { $(this).addClass('focus'); },
+                    function() { $(this).removeClass('focus'); }
+                );
+                $(this).click(function() {
+                    if ($(this).is('.asc')) {
+                        $(this).removeClass('asc');
+                        $(this).addClass('desc selected');
+                        sortOrder = -1;
+                    }
+                    else {
+                        $(this).addClass('asc selected');
+                        $(this).removeClass('desc');
+                        sortOrder = 1;
+                    }
+                    $(this).siblings().removeClass('asc selected');
+                    $(this).siblings().removeClass('desc selected');
+                    var arrData = $('table').find('tbody >tr:has(td)').get();
+                    arrData.sort(function(a, b) {
+                        var val1 = $(a).children('td').eq(col).text().toUpperCase();
+                        var val2 = $(b).children('td').eq(col).text().toUpperCase();
+                        if($.isNumeric(val1) && $.isNumeric(val2))
+                            return sortOrder == 1 ? val1-val2 : val2-val1;
+                        else
+                            return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
+                    });
+                    $.each(arrData, function(index, row) {
+                        $('tbody').append(row);
+                    });
+                });
+            });
         });
     </script>
 </body>
